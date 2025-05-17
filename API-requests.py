3e15a91a622413a58a22e12cd68ab5e44c728624
Neo Colpin <colpinneo@gmail.com>
2025-05-18 01:46:35 +0200

    fix: folder model rename

--- API-requests.py
+++ API-requests.py
@@ -1,4 +1,4 @@
 import requests
 
-r = requests.post("http://127.0.0.1:8000/predict", json={"text": "very medium product"})
+r = requests.post("http://127.0.0.1:8000/predict", json={"text": "a simple product"})
 print(r.json())

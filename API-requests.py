e8dfc30af53aa768cb6d8645b97b9a60c52ebb4d
Neo Colpin <colpinneo@gmail.com>
2025-05-18 01:21:58 +0200

    fix: folder model

--- API-requests.py
+++ API-requests.py
@@ -1,4 +1,4 @@
 import requests
 
-r = requests.post("http://127.0.0.1:8000/predict", json={"text": "very good product"})
+r = requests.post("http://127.0.0.1:8000/predict", json={"text": "very medium product"})
 print(r.json())
